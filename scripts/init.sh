0de76cc13504a1c30790fbb5ddc73ba4d582a9c7
chenpeel <chgenpeel@foxmail.com>
2025-12-17 15:43:01 +0800

    rm unneed

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /opt/ros/$ROS_DISTRO/setup.bash
+. /opt/ros/$ROS_DISTRO/setup.bash
 colcon build 
-source install/setup.bash
+. install/setup.bash
 ros2 launch websocket_bridge full_system.launch.py
