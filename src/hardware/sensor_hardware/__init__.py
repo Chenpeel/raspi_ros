198479df7c1fb3f0e46d94416adbf4e7a4eb15b0
chenpeel <chgenpeel@foxmail.com>
2025-12-18 19:17:03 +0800

    support uart

--- src/hardware/sensor_hardware/__init__.py
+++ src/hardware/sensor_hardware/__init__.py
@@ -1,4 +1,5 @@
 """传感器硬件驱动模块"""
-from .imu_driver import ImuDriver
+from .imu_driver import ImuDriver  # I2C 版本
+from .imu_serial_driver import ImuSerialDriver  # 串口版本
 
-__all__ = ['ImuDriver']
+__all__ = ['ImuDriver', 'ImuSerialDriver']
