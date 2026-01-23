5599af3d339af717757318c973920021949c2196
chenpeel <chgenpeel@foxmail.com>
2026-01-23 15:42:33 +0800

    fix:test servo

--- scripts/bus_servo.sh
+++ scripts/bus_servo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-for id in [1..15]; do
+for id in {1..15}; do
     for position in 0 45 90 135 180 135 90 45 0; do
-        ros2 topic pub --once /servo/command std_msgs/String "data: '{\"servo_type\":\"bus\",\"servo_id\":${id},\"position\": ${position},\"speed\":50}'"
+        ros2 topic pub --once /servo/command servo_msgs/msg/ServoCommand \
+        "{servo_type: 'bus', servo_id: ${id}, position: ${position}, speed: 50, stamp: {sec: 0, nanosec: 0}}"
         sleep 0.1
     done
 done
